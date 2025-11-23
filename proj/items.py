f8fadabfdd2b4f642b36d0b873747502000d8eb0
root <2047420147@qq.com>
2025-11-23 15:13:14 +0800

    docs📝: 新命名

--- proj/items.py
+++ proj/items.py
@@ -10,6 +10,6 @@ class FileInfoItem(scrapy.Item):
     # 定义你的item字段，就像你在代码中使用的字典键一样
     file_name = scrapy.Field()        # 文件名
     local_path = scrapy.Field()       # 本地存储路径
-    remote_path = scrapy.Field()      # 远程路径（如上传到云存储）
-    file_metadata = scrapy.Field()    # 文件元数据（字典）
+    remote_path = scrapy.Field()      # 远程路径
+    file_metadata = scrapy.Field()    # 文件元数据
     other_metadata = scrapy.Field()   # 其他元数据（字典）
